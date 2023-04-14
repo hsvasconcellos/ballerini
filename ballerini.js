cd807ee9f1495015f77910fa58fdd9d894e46230
hsvasconcellos <61476196+hsvasconcellos@users.noreply.github.com>
2023-04-14 12:47:30 -0300

    responsividade ballerini1

---
+++ ballerini.js
@@ -0,0 +1,14 @@
+var meuMenu = document.getElementById("listamenu");
+
+var botao = document.getElementById("burguer");
+
+botao.addEventListener('click', clickMenu);
+
+function clickMenu() {
+  if (meuMenu.style.display == 'block') {
+        meuMenu.style.display = 'none';
+    } else {
+        meuMenu.style.display = 'block'
+    }
+}
+
