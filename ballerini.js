a9f9ad9299cba51d4f34103c5820c5e593adef17
hsvasconcellos <61476196+hsvasconcellos@users.noreply.github.com>
2023-04-14 23:49:43 -0300

    ballerini15

--- ballerini.js
+++ ballerini.js
@@ -5,11 +5,7 @@ botao.addEventListener("click", clickMenu);
 
 function clickMenu() {
   if (meuMenu.style.display == "block") {
-    if (window.innerWidth < 900) {
-      meuMenu.style.display = "none";
-    } else {
-      meuMenu.style.display = "block";
-    }
+    meuMenu.style.display = "none";
   } else {
     meuMenu.style.display = "block";
   }
