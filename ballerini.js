2deb3b8ecc710161464c83a68cb3c0f4642b477b
hsvasconcellos <61476196+hsvasconcellos@users.noreply.github.com>
2023-04-14 21:59:04 -0300

    ballerini6

--- ballerini.js
+++ ballerini.js
@@ -1,14 +1,16 @@
 var meuMenu = document.getElementById("listamenu");
-
 var botao = document.getElementById("burguer");
 
-botao.addEventListener('click', clickMenu);
+botao.addEventListener("click", clickMenu);
 
 function clickMenu() {
-  if (meuMenu.style.display == 'block') {
-        meuMenu.style.display = 'none';
+  if (meuMenu.style.display == "block") {
+    if (window.innerWidth < 900) {
+      meuMenu.style.display = "none";
     } else {
-        meuMenu.style.display = 'block'
+      meuMenu.style.display = "block";
     }
+  } else {
+    meuMenu.style.display = "block";
+  }
 }
-
